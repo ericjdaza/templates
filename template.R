b1b567915f6bcd376cc53543e862c09d1ae322ca
Eric J. Daza <ericjdaza@stanford.edu>
2016-06-26 03:13:32 -0700

    Add files via upload

---
+++ template.R
@@ -0,0 +1,113 @@
+# ----- template.R.v160625s ----- #
+
+# ========================== #
+# ========================== #
+# ===== Documentation. ===== #
+# ========================== #
+# ========================== #
+#
+# Project:
+#
+# Description:
+#
+# 160524t [Daza_EJ] --- Created program.
+#   --- item
+#     --- subitem
+#
+
+
+
+
+
+
+
+# ========================================================= #
+# ========================================================= #
+# ===== Preliminary code (e.g., packages, libraries). ===== #
+# ========================================================= #
+# ========================================================= #
+
+
+
+### Clear environment.
+rm(list=ls())
+
+
+
+### Packages to install.
+# install.packages()
+
+
+
+### Random seeds.
+# scalar.seed <- 
+
+
+
+### Path and variable names: Define.
+# path.ejdtools <- "/Users/edaza/Box Sync/My Documents/Programming/R/ejdtools/"
+# path.ejdtools <- "/Users/ericjaydaza/Box Sync/My Documents/Programming/R/ejdtools/"
+
+
+
+### External user-defined functions.
+
+# Define function to construct variable-characteristics codebook.
+# if(!exists("foo", mode="function")) source(paste0(path.ejdtools, "ejdtools_charvar.R"))
+
+
+
+
+
+
+
+# ============================ #
+# ============================ #
+# ===== Data Management. ===== #
+# ============================ #
+# ============================ #
+
+
+
+
+
+##### Path and variable names: Define.
+
+
+
+
+
+##### Raw datasets: Read, import, load, etc.
+# rawdata <- 
+
+
+
+
+
+##### Analysis datasets: Construct, export, etc.
+# analdata <- 
+# write.table(analdata, )
+
+
+
+
+
+
+
+# ===================== #
+# ===================== #
+# ===== Analyses. ===== #
+# ===================== #
+# ===================== #
+
+
+
+
+
+
+
+# ====================================================================== #
+# ====================================================================== #
+# ===== Miscellaneous (e.g., code, help files/websites, examples). ===== #
+# ====================================================================== #
+# ====================================================================== #
